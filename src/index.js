f9a21d52a6f5d939cfe165601985acf81a3bb8da
Chris Whong <chris.m.whong@gmail.com>
2022-06-09 20:34:57 -0400

    add sentry.io

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom'
 import {
   BrowserRouter
 } from 'react-router-dom'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 
 import './index.css'
 import AuthContainer from './AuthContainer'
 import reportWebVitals from './reportWebVitals'
+
+Sentry.init({
+  dsn: 'https://f8b04c9dc5574d58b99591006f3647fe@o1282705.ingest.sentry.io/6490591',
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+})
 
 ReactDOM.render(
   <React.StrictMode>
