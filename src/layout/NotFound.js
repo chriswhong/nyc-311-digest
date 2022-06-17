f7e85f879b32855f868bb6ef5fb2b9cf301177f8
Chris Whong <chris.m.whong@gmail.com>
2022-06-17 08:29:35 -0400

    fix 404 display

--- src/layout/NotFound.js
+++ src/layout/NotFound.js
@@ -6,12 +6,12 @@ import Button from '../ui/Button'
 export default function NotFound () {
   const navigate = useNavigate()
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className='absolute top-0 bottom-0 left-0 right-0 z-20 flex items-center justify-center bg-white'>
       <div className='flex items-start'>
-        <h1 className='text-5xl font-bold text-indigo-600 pr-6'>404</h1>
+        <h1 className='pr-6 text-5xl font-bold text-indigo-600'>404</h1>
         <div className=''>
-          <div className='border-l pl-6 mb-8'>
-            <h3 className='text-5xl font-bold mb-2'>Page Not Found</h3>
+          <div className='pl-6 mb-8 border-l'>
+            <h3 className='mb-2 text-5xl font-bold'>Page Not Found</h3>
             <p className='text-gray-500'>Sorry, you tried to load a page that doesn't exist</p>
           </div>
           <div className='pl-6'>
